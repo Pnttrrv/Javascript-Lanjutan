02de0676fcafe6aae2217fb956f610c07630dad8
pnttrrv <ndhkptr@gmail.com>
2025-02-11 00:53:29 +0700

    still filter, map, and reduce

--- Filter,map,reduce/script2.js
+++ Filter,map,reduce/script2.js
@@ -2,15 +2,55 @@ const spanJumlahDurasi = document.querySelector('.total-durasi');
 const spanJumlahVid = document.querySelector('.jumlah-video');
 const jl = Array.from(document.querySelectorAll('[data-duration]'));
 
-const jumlahvid = [];
-jl.forEach((text) => {
-  if (text.innerHTML.includes('Javascript')) {
-    jumlahvid.push(text);
-  }
-});
+const jumlahvid = jl.filter((e) => e.innerHTML.includes('Javascript'));
+console.log(jumlahvid);
+
+// // jl.forEach((text) => {
+// //   if (text.innerHTML.includes('Javascript')) {
+// //     jumlahvid.push(text);
+// //   }
+// // });
 
 spanJumlahVid.innerHTML = jumlahvid.length;
-console.log(jumlahvid);
+
+let arrDurasi = jl
+  .filter((e) => e.innerHTML.includes('Javascript'))
+  .map((e) => e.dataset.duration)
+  .map((waktu) => {
+    const parts = waktu.split(':').map((part) => parseFloat(part));
+    return parts[0] * 60 + parts[1];
+  })
+  .reduce((a, b) => a + b);
+
+let menit = (arrDurasi / 60).toFixed(0);
+let jam = (menit / 60).toFixed(0);
+let totmen = (menit / 60 - jam).toFixed(2);
+// let bulat = jam.toFixed(2);
+console.log(totmen);
+
+spanJumlahDurasi.innerHTML = `${jam} jam, ${totmen} menit`;
+
+// let waktu = jl
+//   .filter((e) => e.innerHTML.includes('Javascript'))
+//   .map((e) => e.dataset.duration)
+//   .map((time) => {
+//     const parts = time.split(':');
+//     let jumlahTime = parseInt(parts[0] * 60) + parseInt(parts[1]);
+//     console.log(parts);
+//     console.log(jumlahTime);
+//   });
+
+// console.log('==========');
+
+// let jumlahWaktu = jl
+//   .filter((e) => e.innerHTML.includes('Javascript'))
+//   .map((e) => e.dataset.duration)
+//   .map((time) => {
+//     const parts = time.split(':').map((part) => parseFloat(part));
+//     let jumlahTime = parts[0] * 60 + parts[1];
+//     console.log(jumlahTime);
+//   });
+//   .reduce((menit, detik) => menit + detik);
 
 // let arrDurasi = [];
 // let totDurasi = jl.forEach((atri) => {
