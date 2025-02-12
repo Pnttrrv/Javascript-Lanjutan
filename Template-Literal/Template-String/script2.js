eb19d011d715f39cff79a964c87eb91bf544ccf2
pnttrrv <ndhkptr@gmail.com>
2025-02-13 00:00:45 +0700

    tagged template literal

--- Template-Literal/Template-String/script2.js
+++ Template-Literal/Template-String/script2.js
@@ -36,16 +36,54 @@
 //   .join('')}
 // </div>`;
 
-const lagu = {
-  judul: 'tetap dalam jiwa',
-  penyanyi: 'isyana',
-  feat: 'rayi',
-};
+// const lagu = {
+//   judul: 'tetap dalam jiwa',
+//   penyanyi: 'isyana',
+//   feat: 'rayi',
+// };
 
-const ol = `<div class="lagu">
-<ul>
-<li>${lagu.judul}</li>
-<li>${lagu.penyanyi} ${lagu.feat ? `feat ${lagu.feat}` : ''}</li>
-</ul>
-</div>`;
-document.body.innerHTML = ol;
+// const ol = `<div class="lagu">
+// <ul>
+// <li>${lagu.judul}</li>
+// <li>${lagu.penyanyi} ${lagu.feat ? `feat ${lagu.feat}` : ''}</li>
+// </ul>
+// </div>`;
+
+// nestef html fragment
+
+// const mhs = {
+//   nama: 'dika',
+//   semester: 5,
+//   matkul: ['rpl', 'matdis', 'algo', 'pbo'],
+// };
+
+// function cetakMataKuliah(matakuliah) {
+//   return ` <ul>
+//     ${matakuliah.map((mk) => `<li>${mk}</li>`).join('')}
+
+//     </ul>`;
+// }
+// const ol = `<div class="mhs">
+// <h2>${mhs.nama}</h2>
+// <span class="mhs">${mhs.semester}</span>
+// <h4>Mata Kuliah</h4>
+
+// ${cetakMataKuliah(mhs.matkul)}
+
+// </div>`;
+// document.body.innerHTML = ol;
+
+const nama = 'Dika';
+const umur = 27;
+
+function coba(strings, ...values) {
+  //   let str = '';
+  //   strings.forEach((Text, i) => {
+  //     str += `${Text}${values[i] || ''}`;
+  //   });
+  return strings.reduce((result, str, i) => `${result}${str}${values[i] || ''}`, '');
+  //   console.log(str);
+}
+
+let str = coba`Halo nama saya ${nama}, saya ${umur} tahun`;
+console.log(str);
